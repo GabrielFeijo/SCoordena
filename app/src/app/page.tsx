b0df358d61e35b76079d8cdf95c94dac2b5092c4
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-06-18 01:57:36 -0300

    refactor: extract user avatar into separate component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,27 +2,23 @@ import Menu from '@/components/dashboard/menu';
 import WelcomeBanner from '@/components/welcome-banner';
 import { ModeToggle } from '@/components/mode-toogle';
 import CreateEvent from '@/components/dashboard/create-event';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import AvatarComponent from '@/components/dashboard/avatar';
 
 export default function Home() {
 	return (
-		<main className='flex m-6 h-[calc(100vh-3rem)] space-x-6'>
+		<main className='flex m-4 h-[calc(100vh-2rem)] space-x-4'>
 			<Menu />
-
-			<div className='grid grid-cols-12 gap-4'>
-				<div className='col-span-7'>
+			<div className='grid grid-cols-1 lg:grid-cols-12 gap-4 w-full h-fit'>
+				<div className='lg:col-span-7 space-y-4'>
 					<WelcomeBanner />
 				</div>
-				<div className='col-span-5 space-y-4'>
-					<div className='flex justify-end space-x-4'>
+				<div className='lg:col-span-5 space-y-4'>
+					<div className='flex space-x-4'>
+						<p className='w-full text-xl font-semibold'>
+							Here is your dashboard, Gabriel
+						</p>
 						<ModeToggle />
-						<Avatar>
-							<AvatarImage
-								src='https://github.com/shadcn.png'
-								alt='@shadcn'
-							/>
-							<AvatarFallback>CN</AvatarFallback>
-						</Avatar>
+						<AvatarComponent />
 					</div>
 					<CreateEvent />
 				</div>
--- app/src/components/welcome-banner.tsx
+++ app/src/components/welcome-banner.tsx
@@ -1,11 +1,11 @@
 const WelcomeBanner = () => {
 	return (
-		<section className='flex items-center bg-secondary rounded-xl h-fit w-full overflow-hidden p-1'>
-			<div className='px-9'>
+		<section className='flex justify-between items-center bg-secondary rounded-xl h-full w-full overflow-hidden p-1'>
+			<div className='px-9 py-9 lg:py-0'>
 				<h2 className='text-4xl'>Hello Gabriel!</h2>
 				<p>It’s good to see you again.</p>
 			</div>
-			<div className='bg-[url("/back.png")] bg-repeat h-32 w-72 rounded-r-xl'></div>
+			<div className='bg-[url("/back.png")] bg-repeat h-full w-[45%]  rounded-r-xl'></div>
 		</section>
 	);
 };
