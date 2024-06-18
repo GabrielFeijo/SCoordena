8d0e1480f71c8ecf0f58988b24cb8eb9bc343625
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-06-18 12:51:15 -0300

    feat: add last events component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Menu from '@/components/dashboard/menu';
 import WelcomeBanner from '@/components/welcome-banner';
 import { ModeToggle } from '@/components/mode-toogle';
@@ -5,6 +6,55 @@ import CreateEvent from '@/components/dashboard/create-event';
 import AvatarComponent from '@/components/dashboard/avatar';
 import CalendarComponent from '@/components/dashboard/calendar-component';
 import Card from '@/components/dashboard/card';
+import {
+	Bar,
+	BarChart,
+	CartesianGrid,
+	Legend,
+	ResponsiveContainer,
+	Tooltip,
+	XAxis,
+	YAxis,
+} from 'recharts';
+import LastEvents from '@/components/dashboard/last-events';
+
+const data = [
+	{
+		name: 'Page A',
+		uv: 4000,
+		pv: 2400,
+	},
+	{
+		name: 'Page B',
+		uv: 3000,
+		pv: 1398,
+	},
+	{
+		name: 'Page C',
+		uv: 2000,
+		pv: 3800,
+	},
+	{
+		name: 'Page D',
+		uv: 2780,
+		pv: 3908,
+	},
+	{
+		name: 'Page E',
+		uv: 1890,
+		pv: 4800,
+	},
+	{
+		name: 'Page F',
+		uv: 2390,
+		pv: 3800,
+	},
+	{
+		name: 'Page G',
+		uv: 3490,
+		pv: 4300,
+	},
+];
 
 export default function Home() {
 	return (
@@ -18,7 +68,7 @@ export default function Home() {
 						<WelcomeBanner />
 					</div>
 					<div className='lg:col-span-5 space-y-4'>
-						<div className='flex space-x-4'>
+						<div className='flex items-center space-x-4'>
 							<p className='w-full text-xl font-semibold'>
 								Here is your dashboard, Gabriel
 							</p>
@@ -45,12 +95,30 @@ export default function Home() {
 							/>
 						</div>
 						<h2 className='text-2xl '>Your statistics</h2>
+						<div className='bg-secondary rounded-xl p-4 h-[calc(100vh-21.5rem)]'>
+							<ResponsiveContainer
+								width='100%'
+								height='100%'
+							>
+								<BarChart data={data}>
+									<XAxis dataKey='name' />
+									<YAxis />
+									<Tooltip />
+									<Bar
+										dataKey='pv'
+										fill='#8884d8'
+									/>
+									<Bar
+										dataKey='uv'
+										fill='#82ca9d'
+									/>
+								</BarChart>
+							</ResponsiveContainer>
+						</div>
 					</div>
 					<div className='lg:col-span-5 space-y-4'>
 						<CalendarComponent />
-						<CalendarComponent />
-						<CalendarComponent />
-						<CalendarComponent />
+						<LastEvents />
 					</div>
 				</div>
 			</div>
---
+++ app/src/components/dashboard/last-events.tsx
@@ -0,0 +1,50 @@
+const LastEvents = () => {
+	return (
+		<div className='bg-secondary p-4 rounded-xl '>
+			<div className='h-[calc(100vh-38.5rem)] overflow-y-auto'>
+				<h3 className='text-lg font-semibold'>Last Events</h3>
+				<p className='text-sm text-muted-foreground'>
+					This is where you can see your last events
+				</p>
+				<ul className='space-y-1 mt-2'>
+					<li>Senac Aprende</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+					<li>Senac Cursos</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default LastEvents;
