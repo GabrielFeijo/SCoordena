51105ff124a62d69438c5f64b4455eafb841d0f8
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-06-18 16:13:09 -0300

    feat: initial Fastify setup

---
+++ fastify-api/src/app.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify';
+
+const server = Fastify();
+
+server.get('/helloworld', async (req, res) => {
+	return { message: 'Hello World!' };
+});
+
+async function main() {
+	try {
+		await server.listen({ port: 3333, host: '0.0.0.0' });
+		console.log('Server listening at http://localhost:3333');
+	} catch (error) {
+		console.error(error);
+		process.exit(1); // exit as failure
+	}
+}
+
+main();
