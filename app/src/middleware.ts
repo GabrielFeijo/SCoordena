bd7c6bc271f2177d2bfc4bda5874a870992aa0a3
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-06-25 13:56:13 -0300

    fix: adjust middleware for correct user redirection

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,12 +1,37 @@
-import { withAuth } from 'next-auth/middleware';
+import { Role } from '@prisma/client';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default withAuth({
-	callbacks: {
-		authorized: async ({ req, token }) => {
-			if (req.nextUrl.pathname.startsWith('/dashboard'))
-				return token?.role === 'ADMIN';
-			return !!token;
-		},
-	},
-});
-export const config = { matcher: ['/dashboard'] };
+export async function middleware(request: NextRequest) {
+	const { pathname } = request.nextUrl;
+
+	const token = await getToken({ req: request });
+
+	if (!token) {
+		return NextResponse.redirect(new URL('/api/auth/signin', request.url));
+	}
+
+	if (protectedAdminRoutes.some((route) => pathname.startsWith(route))) {
+		if (token.role !== Role.ADMIN) {
+			return NextResponse.redirect(new URL('/', request.url));
+		}
+	}
+}
+
+const protectedAdminRoutes = [
+	'/dashboard',
+	'/event/create',
+	'/event/schedule',
+	'/documentation',
+];
+
+export const config = {
+	matcher: [
+		'/documentation',
+		'/dashboard',
+		'/events',
+		'/events/:path*',
+		'/event/create',
+		'/event/schedule/:path*',
+	],
+};
