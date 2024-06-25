91dbe867e006b150919432c915930792d0775b56
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-06-25 15:21:13 -0300

    fix: correct text on admin dashboard

--- app/src/components/dashboard/user-component.tsx
+++ app/src/components/dashboard/user-component.tsx
@@ -7,8 +7,9 @@ const UserComponent = async () => {
 
 	return (
 		<div className='flex items-center space-x-4'>
-			<p className='w-full xl:text-xl lg:text-base md:text-xs text-xs font-semibold !whitespace-nowrap'>
-				Here is your, {session?.user?.name?.split(' ')[0] || 'Anonymous'}
+			<p className='w-full xl:text-xl lg:text-base  font-semibold !whitespace-nowrap'>
+				Here is your dashboard,
+				{session?.user?.name?.split(' ')[0] || 'Anonymous'}
 			</p>
 			<ModeToggle />
 			<AvatarComponent imageUrl={session?.user?.image} />
